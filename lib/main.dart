fa9d1eacb8828aa73412b83efe533ba1cb314ef7
Famcy <Famcydaware@gmail.com>
2021-06-05 05:06:48 -0700

    created email and password

--- lib/main.dart
+++ lib/main.dart
@@ -49,9 +49,59 @@ class _MyHomePageState extends State<MyHomePage>{
               ]
             ),
           ),
-        ]
+            Container(
+            padding: EdgeInsets.only(top:35.0, left:20.0, right:20.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration:InputDecoration(
+                    labelText:'EMAIL',
+                    labelStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide:BorderSide (color:Colors.blue)
+                    )
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                 TextField(
+                  decoration:InputDecoration(
+                    labelText:'PASSWORD',
+                    labelStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide:BorderSide (color:Colors.blue)
+                    )
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 5.0),
+                Container(
+                  alignment: Alignment(1.0, 0.0),
+                  padding: EdgeInsets.only(top:15.0, left:20.0),
+                  child: InkWell(
+                    child: Text('FORGOT PASSWORD?',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat',
+                      decoration: TextDecoration.underline
+                    ),
+                    ),
+                  ),
+
+                ),
+        ]),
 
 
+            ),],
       ),
     );
   }}
+  
