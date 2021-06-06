1c578b894dc2c38b9e8c465e556a59522786953f
rahmahassan <rahmahassan0102@gmail.com>
2021-06-06 23:41:38 +0100

    change  color detail input request

--- lib/signup.dart
+++ lib/signup.dart
@@ -52,7 +52,7 @@ class _SignupPageState extends State<SignupPage>{
                     labelStyle: TextStyle(
                       fontFamily: 'Montserrat',
                       fontWeight: FontWeight.bold,
-                      color: Colors.grey
+                      color: Colors.blue[900]
                     ),
                     focusedBorder: UnderlineInputBorder(
                       borderSide:BorderSide (color:Colors.blue)
@@ -67,7 +67,7 @@ class _SignupPageState extends State<SignupPage>{
                     labelStyle: TextStyle(
                       fontFamily: 'Montserrat',
                       fontWeight: FontWeight.bold,
-                      color: Colors.grey
+                      color: Colors.blue[900]
                     ),
                     focusedBorder: UnderlineInputBorder(
                       borderSide:BorderSide (color:Colors.blue)
